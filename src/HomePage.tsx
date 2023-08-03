1567f792b875a65cc935e6cf45ac052ba2de5660
lukemcelligott <lpm1006@gmail.com>
2023-08-03 16:48:48 -0400

    setup

---
+++ src/HomePage.tsx
@@ -0,0 +1,13 @@
+/* Show welcome information when user visits site */
+
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function HomePage() {
+    return (
+        <p>hello</p>
+    )
+}
+
+export default HomePage;
